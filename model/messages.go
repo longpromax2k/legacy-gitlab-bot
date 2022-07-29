d0dbb49e5da4c7a3dc3ad7076a327c2b9c043701
natalyst208 <tuandeptrailtt@gmail.com>
2022-07-29 11:39:08 +0700

    MergeRequest

--- model/messages.go
+++ model/messages.go
@@ -1,5 +1,6 @@
 package model
 
 var (
-	PushEventMsg = "*%s* pushed to branch *%s* in [%s/%s](%s) \n*commit*: `%s`"
+	PushEventMsg          = "*%s* pushed to branch *%s* in [%s/%s](%s) \n*commit*: `%s`"
+	MergeRequestEventsMsg = "*%s* created in branch *%s* in [%s/%s](%s) \n* a merge request*"
 )
