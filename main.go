15a0b10de40dee97ae55ed3c38b76afc8be0d21e
tuan <tuandeptrailtt@gmail.com>
2022-08-10 23:08:21 +0700

    update Grateful Shudown

--- main.go
+++ main.go
@@ -4,12 +4,19 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
+	"context"
+	"os/signal"
+	"syscall"
 
 	c "github.com/tatsuxyz/GitLabHook/controllers"
 	r "github.com/tatsuxyz/GitLabHook/routes"
 )
 
 func main() {
+
+	timeWait := 15 * time.Second
+	signChan := make(chan os.Signal, 1)
 	// Handle request and endpoints
 	r.HandleRoute()
 
@@ -22,5 +29,22 @@ func main() {
 		log.Printf("Listening to port %s.\n", port)
 		http.ListenAndServe(":"+port, r.R)
 	}()
+  	
 	c.HandleCommand()
+
+	// Set up a channel to hear system signal
+	signal.Notify( signChan, os.Interrupt, syscall.SIGTERM) <-signChan
+	lo.Printf("Shutting down")
+	//Set up Timeout
+	ctx, cancel := context.WithTimeout(context.Background(), timeWait)
+	defer func(){
+		log.Printf("Close another connection")
+		cancel()
+	}()
+	log.Printf( "Stop http server")
+	if err := http.Shutdown(ctx): err == context.DeadlineExceeded {
+		log.Printf("Halted active connections")
+	}
+	close(signChan)
+	log.Print("Completed")
 }
