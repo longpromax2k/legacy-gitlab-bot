55e6ec855a95a3ab53468f93968769404b7fc1b8
long <hoanglong.letter@gmail.com>
2022-08-23 10:42:13 +0700

    fix: a lot of things, kinda :)

--- controllers/commands.go
+++ controllers/commands.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os"
 	"strconv"
 
 	tgbot "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -15,42 +14,35 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-type User struct {
-	ID primitive.ObjectID `bson:"_id" json:"id,omitempty"`
-}
-
 var chatId string
 
 func CommandStart(up *tgbot.Update, msg *tgbot.MessageConfig) {
 	chatId = strconv.Itoa(int(up.Message.Chat.ID))
-	var re User
-	cid := strconv.Itoa(int(up.Message.Chat.ID))
-	//TODO: check existed data
-	err := h.Col.FindOne(context.TODO(), bson.D{{Key: "chatId", Value: "cid"}}).Decode(&re)
-	//err.Decode(&re)
+	var r bson.M
+
+	err := h.GroupCol.FindOne(context.TODO(), bson.D{{Key: "chatId", Value: chatId}}).Decode(&r)
 	if err != mongo.ErrNoDocuments {
-		log.Printf("Document existed.\n")
-		msg.Text = fmt.Sprintf(model.ChatExistMsg, os.Getenv("HOST_URL"), re.ID)
+		msg.Text = model.ChatExistMsg
 		return
 	}
 
-	// TODO: insert new value into database
-	doc := bson.D{{Key: "chatId", Value: cid}}
-	result, _ := h.Col.InsertOne(context.TODO(), doc)
-	oid := result.InsertedID.(primitive.ObjectID)
-	log.Printf("Inserted doc with id:%v.\n", result.InsertedID)
-	msg.Text = fmt.Sprintf(model.ChatInsertMsg, os.Getenv("HOST_URL"), oid.Hex())
+	doc := bson.D{{Key: "chatId", Value: chatId}}
+	res, err := h.GroupCol.InsertOne(context.TODO(), doc)
+	if err != nil {
+		log.Fatal(err)
+	}
+	oid := res.InsertedID.(primitive.ObjectID)
 
+	msg.Text = fmt.Sprintf(model.ChatInsertMsg, h.HostUrl, h.UrlPath, oid.Hex())
 }
 
 func CommandDrop(up *tgbot.Update, msg *tgbot.MessageConfig) {
 	chatId = strconv.Itoa(int(up.Message.Chat.ID))
 
-	// TODO
-	filter := bson.D{{Key: "chatId", Value: chatId}}
-	_, err := h.Col.DeleteOne(context.TODO(), filter)
-	if err != nil {
-		panic(err)
+	f := bson.D{{Key: "chatId", Value: chatId}}
+	if _, err := h.GroupCol.DeleteOne(context.TODO(), f); err != nil {
+		log.Panic(err)
 	}
+
 	msg.Text = model.ChatDropMsg
 }
